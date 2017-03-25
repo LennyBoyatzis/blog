6971295354de0bae70ac8ccb3396eac67c5ee36f
Lenny Boyatzis <lennyboyatzis@gmail.com>
2017-03-25 11:22:13 +1100

    Code font change and content change

--- src/js/code-snippets/lc-vs-ge.js
+++ src/js/code-snippets/lc-vs-ge.js
@@ -1,9 +1,11 @@
 const lcVsGeSnippet = `
   # List Comprehension [...]
-  list_comp = [i for i in range(1000)]
+  list_comp = [i for i in range(5)] 
+  # [0,1,2,3,4]
 
   # Generator Expressions (...)
-  gen_exp = (i for i in range(1000))
+  gen_exp = (i for i in range(5)) 
+  # <generator object <genexpr> at 0x101c01e08
 `
 
 const memoryDifference = `
@@ -13,6 +15,13 @@ const memoryDifference = `
   getsizeof(list_comp) # 9024 bytes
 
   gen_exp = (i for i in range(1000))
+  getsizeof(gen_exp) # 88 bytes
+
+  # Lets try something a little bigger!
+  list_comp = [x * 5 for x in range(5000)]
+  getsizeof(list_comp) # 43040 bytes
+
+  gen_exp = (i for i in range(5000))
   getsizeof(gen_exp) # 88 bytes
 `
 export { lcVsGeSnippet, memoryDifference };
--- src/js/containers/post-3.js
+++ src/js/containers/post-3.js
@@ -8,10 +8,11 @@ export default class Post extends Component {
   render() {
     return (
       <div className="post-container">
-        <h1 className="post-content post-headline">List comprehension vs Generator Expressions</h1>
+        <h1 className="post-content post-headline">List Comprehension vs Generator Expressions</h1>
         <p className="post-content">
-          A List Comprehension executes immediately and creates a list in memory whereas a Generator Expression returns an object that
-          can be iterated over.
+          Both list comprehensions and generator expressions look very similar in their syntax. 
+          The subtle difference being [...] vs (...).
+          If you print both you’ll see that what is being created under the hood is actually very different. 
         </p>
         <div className="code-sample">
           <Highlight className="python">
@@ -19,21 +20,28 @@ export default class Post extends Component {
           </Highlight>
         </div>
         <p className="post-content">
-          Once the answer has been found they can write it down so that if they get asked the same question again, 
-          they can simply look it up without having to do any hard work.
+          What you can see above is that a list comprehension executes immediately and creates a list in memory, 
+          whereas a generator expression returns a special object that can be iterated over.
         </p>
         <p className="post-content">
-          A simple implementation of a memoize function can be seen in this code snippet below.
+          Even though list comprehension consumes more memory, it is preferable to use when list specific operations such as indexing or slicing 
+          will be required. These operations are not available on generator expressions. Additionally, if a sequence needs
+          to be iterated over multiple times it may be more appropriate to use list comprehension as iterating over a list is quicker than iterating over
+          a generator object.
         </p>
         <p className="post-content">
-          The main feature of a generator is evaluating the elements on demand. This has the advantage of taking up much
-          less memory. This can be verified using the getsizeof module.
+          Using the getsizeof module we can see that as the sequence increases from 1000 items to 5000 items, unlike the list comprehension,
+          there is no change in the size the generator expression.
         </p>
         <div className="code-sample">
           <Highlight className="python">
             {memoryDifference}
           </Highlight>
         </div>
+        <p className="post-content">
+          Generators are helpful for situations where you are working with large sequences of data that
+          you may not want to store in memory but instead generate on the fly. 
+        </p>
       </div>
     );
   }
